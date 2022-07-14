25aaaf42773e25944a2012303c56c1d6fb73235b
yk8710 <y.kaneko198710@gmail.com>
2022-07-14 11:37:58 +0900

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: {with: VALID_PASSWORD_REGEX,  message:'Include both letters and numbers'}
+  validates :password, format: {with: VALID_PASSWORD_REGEX,  message:'is invalid.Include both letters and numbers'}
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid.Input full-width katakana characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: "is invalid.Input full-width katakana characters" }
   validates :birthday, presence: true
 end
