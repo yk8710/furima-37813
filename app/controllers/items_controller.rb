0eafdaa82a23cea40aba4e579fd3ee0295fd1ba6
yk8710 <y.kaneko198710@gmail.com>
2022-07-16 17:23:32 +0900

    コントローラーにnewアクション、createアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,21 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :explain, :item_status_id, :shipping_cost_id, :shipping_date_id, :category_id, :prefecture_id).merge(user_id: current_user_id)
+  end
 end
