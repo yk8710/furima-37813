c743d5b588754b8016a6f601317f64c2b9dd4f60
yk8710 <y.kaneko198710@gmail.com>
2022-07-16 11:13:49 +0900

    アソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: {with: VALID_PASSWORD_REGEX,  message:'is invalid.Include both letters and numbers'}
   validates :nickname, presence: true
