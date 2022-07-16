701a2c893ffc907137a41bc01519a37973dbdd49
yk8710 <y.kaneko198710@gmail.com>
2022-07-16 14:33:40 +0900

    Itemモデルにバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :category_id, numericality: { other_than: 0, message: "Category can't be blank"}
+  validates :item_status_id, numericality: { other_than: 0, message: "Item status can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 0, message: "Prefecture can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 0, message: "Shipping cost can't be blank" }
+  validates :shipping_date_id, numericality: { other_than: 0, message: "Shipping date can't be blank"}
 end
