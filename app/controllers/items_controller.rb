2de0809fd254a48cb2abe47c9eb16325551934ee
yk8710 <y.kaneko198710@gmail.com>
2022-07-16 19:18:40 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,7 +16,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  private
+
   def item_params
-    params.require(:item).permit(:image, :name, :price, :explain, :item_status_id, :shipping_cost_id, :shipping_date_id, :category_id, :prefecture_id).merge(user_id: current_user_id)
+    params.require(:item).permit(:image, :name, :price, :explain, :item_status_id, :shipping_cost_id, :shipping_date_id, :category_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
