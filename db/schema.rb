26682a404ee80327e4921dec3d73250bc774471c
yk8710 <y.kaneko198710@gmail.com>
2022-07-16 11:03:40 +0900

    商品出品情報を保存するためのテーブル追加
    追記した後、rails db:migrateも実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_100846) do
+ActiveRecord::Schema.define(version: 2022_07_16_013956) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "explain", null: false
+    t.integer "item_status_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "category_id", null: false
+    t.integer "prefecture_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
