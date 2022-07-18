eee4581e1f38e0113a928039185f2a5b3a8478eb
yk8710 <y.kaneko198710@gmail.com>
2022-07-18 14:33:08 +0900

    itemモデルのバリデーション記述追記、修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,35 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
+
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+
+
   has_one_attached :image
 
-  validates :category_id, numericality: { other_than: 0, message: "Category can't be blank"}
-  validates :item_status_id, numericality: { other_than: 0, message: "Item status can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 0, message: "Prefecture can't be blank"}
-  validates :shipping_cost_id, numericality: { other_than: 0, message: "Shipping cost can't be blank" }
-  validates :shipping_date_id, numericality: { other_than: 0, message: "Shipping date can't be blank"}
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name 
+    validates :explain
+    validates :category_id
+    validates :item_status_id
+    validates :prefecture_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :item_status_id
+    validates :prefecture_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+  end
 end
