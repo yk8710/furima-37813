d0e135bc7d9cd97f1cb83f75a227d8d413871ea1
yk8710 <y.kaneko198710@gmail.com>
2022-07-20 18:30:34 +0900

    コントローラー修正
    updateアクションもshowアクション,editアクションと同様の記述があるのでbefore_actionにリファクタリング。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :ensure_current_user, only: :edit
 
   def index
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
