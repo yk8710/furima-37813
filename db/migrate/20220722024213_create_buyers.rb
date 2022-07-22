8b2d868e18894d26fa76821a9fb1b3dc310c1698
yk8710 <y.kaneko198710@gmail.com>
2022-07-22 11:50:45 +0900

    Buyerモデル作成

---
+++ db/migrate/20220722024213_create_buyers.rb
@@ -0,0 +1,14 @@
+class CreateBuyers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :buyers do |t|
+      t.references :order , null: false, foreign_key:true
+      t.string :postal_code, null: false
+      t.integer :prefecture, null: false
+      t.string :city, null: false
+      t.string :block, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_021458) do
+ActiveRecord::Schema.define(version: 2022_07_22_024213) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2022_07_22_021458) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "postal_code", null: false
+    t.integer "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_buyers_on_order_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2022_07_22_021458) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "buyers", "orders"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
 end
