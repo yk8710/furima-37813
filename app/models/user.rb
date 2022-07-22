281e0ddebdc7633865b99e99339e094ec9cd28a1
yk8710 <y.kaneko198710@gmail.com>
2022-07-22 15:30:12 +0900

    ordersコントローラー作成、購入ボタン遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid.Include both letters and numbers' }
