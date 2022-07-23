e1452010eff12735c93e6480ef53fdf265c95e2b
yk8710 <y.kaneko198710@gmail.com>
2022-07-23 11:59:43 +0900

    コントローラーとビューファイルの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @order_buyer = OrderBuyer.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    binding.pry
+    @order_buyer = OrderBuyer.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_buyer.valid?
+      @order_buyer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_buyer).permit(:postal_code, :prefecture, :city, :block, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
